b60a3534c05a8022e29ea6f4e411747eee76e780
황병규 <128976098+banggi0323@users.noreply.github.com>
2025-04-21 04:42:03 +0900

    Add Python File [hw6.py]

---
+++ hw6.py
@@ -0,0 +1,19 @@
+i=1
+while i<=9:
+    n=2
+    while n<=5:
+        print(f'{n} x {i} = {n*i:2d}', end='\t')
+        n+=1
+    print()
+    i+=1
+    
+print()
+
+i=1
+while i<=9:
+    n=6
+    while n<=9:
+        print(f'{n} x {i} = {n*i:2d}', end='\t')
+        n+=1
+    print()
+    i+=1
